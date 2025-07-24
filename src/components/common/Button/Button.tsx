e7bbd5d66bf966993e6bf90eca46abe7b1d07900
Franklin Joseph <franklinjoseph360@gmail.com>
2025-07-24 19:06:22 +0530

    fix: remove elevation & ripple effects in button component

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,7 +1,7 @@
 import { Button as MUIButton, type ButtonProps } from '@mui/material'
 
 const Button = (props: ButtonProps) => {
-  return <MUIButton variant="contained" {...props} />
+  return <MUIButton variant="contained" disableElevation disableRipple {...props} />
 }
 
 export default Button
