56c366a8d44ada8ebd1faab42f0c86a053b6de6b
Franklin Joseph <franklinjoseph360@gmail.com>
2025-07-25 23:51:58 +0530

    feat: update preview background color

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -6,19 +6,29 @@ import '../src/index.css'
 
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { Box } from '@mui/material'
 
 const preview: Preview = {
-  decorators: [
-    (Story) => {
-      return (
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <Story />
-          </LocalizationProvider>
-        </ThemeProvider>
-      )
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'dark', value: '#202124' },
+        { name: 'light', value: '#ffffff' },
+      ],
     },
+  },
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <Box sx={{ bgcolor: '#202124', minHeight: '100vh', padding: 2 }}>
+            <Story />
+          </Box>
+        </LocalizationProvider>
+      </ThemeProvider>
+    ),
   ],
 }
 
