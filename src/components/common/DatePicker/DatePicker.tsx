2af2e70c51addde47704d7cd3291a3d8dca00f8b
Franklin Joseph <franklinjoseph360@gmail.com>
2025-07-25 09:45:20 +0530

    feat: fix datepicker UI

--- src/components/common/DatePicker/DatePicker.tsx
+++ src/components/common/DatePicker/DatePicker.tsx
@@ -1,13 +1,14 @@
 import { DatePicker as MUIDatePicker } from '@mui/x-date-pickers/DatePicker'
 import type { TextFieldProps } from '@mui/material'
+import type { DateValidationError, PickerChangeHandlerContext } from '@mui/x-date-pickers/models'
 
-interface Props<TDate> {
-  value: TDate | null
-  onChange: (date: TDate | null) => void
+interface Props {
+  value: Date | null
+  onChange: (date: Date | null, context?: PickerChangeHandlerContext<DateValidationError>) => void
   placeholder?: string
 }
 
-const DatePicker = <TDate,>({ value, onChange, placeholder }: Props<TDate>) => {
+const DatePicker = ({ value, onChange, placeholder }: Props) => {
   return (
     <MUIDatePicker
       value={value}
@@ -19,6 +20,14 @@ const DatePicker = <TDate,>({ value, onChange, placeholder }: Props<TDate>) => {
           placeholder,
           InputProps: {
             disableUnderline: true,
+          },
+          sx: {
+            backgroundColor: '#202124',
+            borderRadius: '8px',
+            input: { color: '#fff' },
+            '& .MuiInputBase-root:before': {
+              borderBottom: 'none',
+            },
           },
         } as Partial<TextFieldProps>,
       }}
