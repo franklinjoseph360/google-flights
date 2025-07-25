6cf292de3cb3edbdce0276cf23ec4a8584785d77
Franklin Joseph <franklinjoseph360@gmail.com>
2025-07-25 17:59:03 +0530

    feat: update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import TextField from '@mui/material/TextField'
 import './App.css'
+import FlightSearchForm from './features/FlightSearch/components/FlightSearchForm'
+import { FlightSearchProvider } from './features/FlightSearch/context/context'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 
 function App() {
   return (
     <div className="app-container">
       <h1>Google Flights - Franklin Joseph</h1>
-      <TextField label="Test" placeholder="Should look styled" />
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <FlightSearchProvider>
+          <FlightSearchForm />
+        </FlightSearchProvider>
+      </LocalizationProvider>
     </div>
   )
 }
